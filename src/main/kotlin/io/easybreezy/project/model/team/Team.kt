5c5ca2f792e3bb975594fe0600320f7d143ff389
shustrik <vadim.golodko@gmail.com>
2020-01-08 22:56:32 +0300

    small changes

--- src/main/kotlin/io/easybreezy/project/model/team/Team.kt
+++ src/main/kotlin/io/easybreezy/project/model/team/Team.kt
@@ -3,15 +3,21 @@ package io.easybreezy.project.model.team
 import io.easybreezy.infrastructure.exposed.dao.AggregateRoot
 import io.easybreezy.infrastructure.exposed.dao.PrivateEntityClass
 import org.jetbrains.exposed.dao.*
+import org.jetbrains.exposed.sql.ResultRow
 import java.util.*
 
-class Team(_project: UUID, _name: String) : AggregateRoot<UUID>(EntityID(UUID.randomUUID(), Teams)) {
+class Team private constructor(id: EntityID<UUID>) : AggregateRoot<UUID>(id) {
     private var name by Teams.name
     private val repositories by Repository referrersOn Teams.repositories
     private val members by Member referrersOn Teams.members
 
-    init {
-        name = _name
+    companion object : PrivateEntityClass<UUID, Team>(object : Repository() {}) {
+        fun create(name: String): Team {
+            return Team.new {
+                this.name = name
+            }
+        }
+
     }
 
     fun addMember(member: UUID, info: Member.Info, roles: List<Role> = emptyList()) {
@@ -25,19 +31,25 @@ class Team(_project: UUID, _name: String) : AggregateRoot<UUID>(EntityID(UUID.ra
 
     }
 
-    fun addRepository(url: String, type: Repository.Type) {
+    fun addRepository(url: String, type: io.easybreezy.project.model.team.Repository.Type) {
 
     }
 
-    class Repository : UUIDEntity(EntityID(UUID.randomUUID(), Repositories)) {
-        companion object : PrivateEntityClass<UUID, Repository>(object : UUIDEntityClass<Repository>(Repositories) {})
-        enum class Type {
-            GITLAB,
-            GITHUB
+
+    abstract class Repository : EntityClass<UUID, Team>(Teams, Team::class.java) {
+        override fun createInstance(entityId: EntityID<UUID>, row: ResultRow?): Team {
+            return Team(entityId)
         }
-    }
 
-    companion object : PrivateEntityClass<UUID, Team>(object : EntityClass<UUID, Team>(Teams) {})
+    }
+}
+
+class Repository : UUIDEntity(EntityID(UUID.randomUUID(), Repositories)) {
+    companion object : PrivateEntityClass<UUID, Repository>(object : UUIDEntityClass<Repository>(Repositories) {})
+    enum class Type {
+        GITLAB,
+        GITHUB
+    }
 }
 
 object Teams : UUIDTable() {
---
+++ src/main/kotlin/io/easybreezy/project/model/team/TeamRepository.kt
@@ -0,0 +1,4 @@
+package io.easybreezy.project.model.team
+
+class TeamRepository {
+}
