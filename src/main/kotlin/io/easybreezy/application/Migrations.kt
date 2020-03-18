758fb2f3be1dd2f20f6859fbc33e10e8d91bacd9
shustrik <vadim.golodko@gmail.com>
2020-03-19 01:31:35 +0300

    add migrations runner

---
+++ src/main/kotlin/io/easybreezy/application/Migrations.kt
@@ -0,0 +1,27 @@
+package io.easybreezy.application
+
+import org.flywaydb.core.Flyway
+
+@Suppress("unused", "UnusedMainParameter")
+class Migrations {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val provider = SystemConfiguration
+
+            val flyway =
+                Flyway.configure().locations("classpath:io/easybreezy/migrations")
+                    .validateOnMigrate(true)
+                    .outOfOrder(true)
+                    .dataSource(
+                        provider.get("bts.jdbc.url", String::class.java),
+                        provider.getOrNull("bts.jdbc.user", String::class.java),
+                        provider.getOrNull("bts.jdbc.password", String::class.java)
+                    )
+                    .baselineOnMigrate(true)
+                    .load()
+
+            flyway.migrate()
+        }
+    }
+}
