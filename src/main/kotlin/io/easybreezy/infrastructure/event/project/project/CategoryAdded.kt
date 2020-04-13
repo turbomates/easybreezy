1a269499aefe1ca31995860daf36e09fa6cad1ce
lady-ann <fedoruk.anna@gmail.com>
2020-04-13 17:07:03 +0300

    Use file serialization

--- src/main/kotlin/io/easybreezy/infrastructure/event/project/project/CategoryAdded.kt
+++ src/main/kotlin/io/easybreezy/infrastructure/event/project/project/CategoryAdded.kt
@@ -1,18 +1,20 @@
+@file:UseSerializers(UUIDSerializer::class)
 package io.easybreezy.infrastructure.event.project.project
 
 import io.easybreezy.infrastructure.event.Event
 import io.easybreezy.infrastructure.serialization.LocalDateTimeSerializer
 import io.easybreezy.infrastructure.serialization.UUIDSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.UseSerializers
 import java.time.LocalDateTime
 import java.util.UUID
 
 @Serializable
 data class CategoryAdded(
-    @Serializable(with = UUIDSerializer::class) val project: UUID,
-    @Serializable(with = UUIDSerializer::class) val category: UUID,
+    val project: UUID,
+    val category: UUID,
     val name: String,
-    @Serializable(with = UUIDSerializer::class) val parent: UUID?,
+    val parent: UUID?,
     @Serializable(with = LocalDateTimeSerializer::class) val at: LocalDateTime
 ) : Event {
     override val key
--- src/main/kotlin/io/easybreezy/infrastructure/event/project/project/RoleChanged.kt
+++ src/main/kotlin/io/easybreezy/infrastructure/event/project/project/RoleChanged.kt
@@ -1,16 +1,19 @@
+@file:UseSerializers(UUIDSerializer::class)
 package io.easybreezy.infrastructure.event.project.project
 
 import io.easybreezy.infrastructure.event.Event
+import io.easybreezy.infrastructure.serialization.LocalDateSerializer
 import io.easybreezy.infrastructure.serialization.LocalDateTimeSerializer
 import io.easybreezy.infrastructure.serialization.UUIDSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.UseSerializers
 import java.time.LocalDateTime
 import java.util.UUID
 
 @Serializable
 data class RoleChanged(
-    @Serializable(with = UUIDSerializer::class) val project: UUID,
-    @Serializable(with = UUIDSerializer::class) val role: UUID,
+    val project: UUID,
+    val role: UUID,
     val name: String,
     val permissions: List<String>,
     @Serializable(with = LocalDateTimeSerializer::class) val at: LocalDateTime
