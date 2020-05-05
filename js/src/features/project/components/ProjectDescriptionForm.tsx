755103601bef73ff01cb161ea9acbc4b280c5208
unknown <aniball2@rambler.ru>
2020-05-05 11:09:11 +0300

    fix project

--- js/src/features/project/components/ProjectDescriptionForm.tsx
+++ js/src/features/project/components/ProjectDescriptionForm.tsx
@@ -8,7 +8,7 @@ import { FormErrorMap } from "MyTypes";
 interface Props {
   project: Project;
   edit: (form: EditProjectDescriptionRequest) => void;
-  close: (close: boolean) => void;
+  closeProjectDescriptionForm: () => void;
   errors: FormErrorMap;
   loading: boolean;
 }
@@ -18,7 +18,7 @@ export const ProjectDescriptionForm: React.FC<Props> = ({
   errors,
   edit,
   loading,
-  close,
+  closeProjectDescriptionForm,
 }) => {
   const [form] = Form.useForm();
 
@@ -72,7 +72,7 @@ export const ProjectDescriptionForm: React.FC<Props> = ({
             </Button>
           </Col>
           <Col>
-            <Button type="danger" onClick={() => close(false)} loading={loading}>
+            <Button type="danger" onClick={closeProjectDescriptionForm} loading={loading}>
               Cancel
             </Button>
           </Col>
--- js/src/features/project/components/ProjectRoleFormEditRoleName.tsx
+++ js/src/features/project/components/ProjectRoleFormEditRoleName.tsx
@@ -1,5 +1,5 @@
+import React, { useRef } from "react";
 import { Button, Input } from "antd";
-import React from "react";
 import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
 
 import { FormField } from "./ProjectRoleForm";
@@ -7,39 +7,42 @@ import { FormField } from "./ProjectRoleForm";
 interface Props {
   field: FormField;
   fieldIndex: number;
-  createRole: (fieldIndex: number, value: string) => void;
-  editInputRole: (fieldIndex: number) => void;
-  setIndexOfEditableInput: (number: number) => void;
-  inputRef: React.RefObject<Input>;
+  editInputRole: (fieldIndex: number, value: string) => void;
+  closeInputRole: () => void;
 }
 
 export const ProjectRoleFormEditRoleName: React.FC<Props> = ({
   field,
-  fieldIndex,
-  createRole,
   editInputRole,
-  setIndexOfEditableInput,
-  inputRef,
+  closeInputRole,
+  fieldIndex,
 }) => {
+  const inputRef = useRef<Input>(null);
+
   return (
     <td>
       <div className="role-form__input-wrapper">
         <Input
           defaultValue={field.name}
+          onPressEnter={() =>
+            editInputRole(fieldIndex, inputRef.current!.state.value)
+          }
           ref={inputRef}
-          onChange={(event) => createRole(fieldIndex, event.target.value)}
           className="role-form__input"
         />
-        {!!field.id && (
-          <div className="role-form__controls-btn">
-            <Button onClick={() => editInputRole(fieldIndex)} type="primary">
-              <CheckCircleOutlined />
-            </Button>
-            <Button onClick={() => setIndexOfEditableInput(-1)} type="danger">
-              <CloseCircleOutlined />
-            </Button>
-          </div>
-        )}
+        <div className="role-form__controls-btn">
+          <Button
+            onClick={() =>
+              editInputRole(fieldIndex, inputRef.current!.state.value)
+            }
+            type="primary"
+          >
+            <CheckCircleOutlined />
+          </Button>
+          <Button onClick={closeInputRole} type="danger">
+            <CloseCircleOutlined />
+          </Button>
+        </div>
       </div>
     </td>
   );
