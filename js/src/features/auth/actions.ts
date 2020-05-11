ef8697cccac15498e780c59b8589dadd8de2013f
Dmk <dmitry.kushnerov@hotmail.com>
2020-05-11 10:45:34 +0300

    Authorixation rules (#124)
    Co-authored-by: Vadim Golodko <vadim.golodko@gmail.com>

--- js/src/features/auth/actions.ts
+++ js/src/features/auth/actions.ts
@@ -1,4 +1,4 @@
-import { SignInData, User } from "AuthModels";
+import { SignInData, User, RulesMap } from "AuthModels";
 import { createAsyncAction } from "typesafe-actions";
 
 export const signInAsync = createAsyncAction(
@@ -18,3 +18,9 @@ export const checkAuth = createAsyncAction(
   "CHECK_AUTH_SUCCESS",
   "CHECK_AUTH_FAILURE",
 )<undefined, User, undefined>();
+
+export const fetchRulesAsync = createAsyncAction(
+  "FETCH_AUTH_RULES_REQUEST",
+  "FETCH_AUTH_RULES_SUCCESS",
+  "FETCH_AUTH_RULES_FAILURE",
+)<undefined, RulesMap, string>();
--- js/src/store/root-reducer.ts
+++ js/src/store/root-reducer.ts
@@ -2,12 +2,11 @@ import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import { History } from "history";
 
-import auth from "../features/auth/reducer";
+import { reducer as auth } from "../features/auth/reducer";
 import humanResource from "../features/human-resouce/reducer";
 import account from "../features/account/reducer";
 import { reducer as location } from "../features/location/reducer";
 import project from "../features/project/reducer";
-
 
 const rootReducer = (history: History<any>) =>
   combineReducers({
